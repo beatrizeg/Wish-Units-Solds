67b2ead2cccf449f96bcaf6a61721cf361a0d20c
beatrizeg <beatriz.eg92@gmail.com>
2020-10-23 17:38:47 +0200

    Final except minor format changes

--- Product_Sales_BEG.R
+++ Product_Sales_BEG.R
@@ -29,16 +29,11 @@ url_main <- "https://raw.githubusercontent.com/beatrizeg/Wish-Units-Solds/main/s
 dest_file <- "data/main.csv"
 download.file(url_main, destfile = dest_file)
 main <- read_csv("data/main.csv")
-save(main, file = "rdas/main.rda")
 
 url_cat <- "https://raw.githubusercontent.com/beatrizeg/Wish-Units-Solds/main/unique-categories.sorted-by-count.csv"
 dest_file_cat <- "data/cat.csv"
 download.file(url_cat, destfile = dest_file_cat)
 cat <- read_csv("data/cat.csv")
-save(cat, file = "rdas/cat.rda")
-
-load("rdas/main.rda")
-load("rdas/cat.rda")
 
 main <- as.data.frame(main)
 cat <- as.data.frame(cat)
